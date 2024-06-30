20d411787f3516791de5ba25a2503512f4b302b8
Anonymous0418 <laxmikathayat559@gmail.com>
2024-06-30 07:55:49 +0545

    Rock paper scissor game

---
+++ RockPaperScissor.py
@@ -0,0 +1,33 @@
+import random
+
+while True:
+    choice = ["rock", "paper", "scissor"]
+
+    computer = random.choice(choice)
+    player = None
+
+    while player not in choice:
+        player = input("rock, paper or scissor?: ")
+
+    if player == computer:
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Tie!")
+
+    elif player == "paper" and computer == "scissor" or player =="rock" and computer =="paper" or player =="scissor" and computer =="paper":
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Oooopppss:(")
+
+    elif player =="scissor" and computer =="paper" or player =="paper" and computer =="rock" or player =="rock" and computer =="scissor":
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Kangrachulason!")
+
+
+    play_again = input("Play again? (yes/no): ")
+
+    if play_again != "yes":
+        break
+
+print("Well Played!!!")
